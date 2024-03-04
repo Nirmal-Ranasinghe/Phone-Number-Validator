5d4d55f8ca30d1bc6a1caca8459842ae35c77351
Nirmal Ranasinghe <NirmalRa@brownsgroup.com>
2024-03-04 08:57:03 +0530

    Add project files.

---
+++ phoneNumberValidator/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+
+Console.Write("Enter the phone number: ");
+String phoneNumber = Console.ReadLine();
+
+string trimNumber = Regex.Replace(phoneNumber, @"\s", "");
+
+string message = (trimNumber.Length < 0 || trimNumber == null) ? "Please enter your number" : $"You number is {trimNumber}";
+Console.Write(message);
