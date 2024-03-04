21a37a05bef64163888d827a9f7456a555227edc
Nirmal Ranasinghe <NirmalRa@brownsgroup.com>
2024-03-04 11:00:39 +0530

    v.2

--- phoneNumberValidator/Program.cs
+++ phoneNumberValidator/Program.cs
@@ -1,20 +1,41 @@
-﻿using static System.Net.Mime.MediaTypeNames;
+﻿using System;
 using System.Text.RegularExpressions;
-using System.Reflection.Metadata;
-using System;
 
-
-class PhoneNumber {
-    public static void Main(string[] args)
+class Program
+{
+    static void Main(string[] args)
     {
-        Console.Write("Enter the phone number: ");
-        string phoneNumber = Console.ReadLine();
-        string trimNumber = Regex.Replace(phoneNumber, @"\s", "");
-
         string pattern = @"^(0|\+?94)?([0-9]{9})$";
 
-        string standardPhoneNumber = Regex.Replace(trimNumber, pattern, "94$2");
+        string phoneNumber;
+        string trimNumber;
+        string standardPhoneNumber;
 
-        Console.WriteLine("Standardized phone number: " + standardPhoneNumber);
+        while (true)
+        {
+            Console.Write("Enter the phone number (enter 'q' to quit): ");
+            phoneNumber = Console.ReadLine().Trim();
+
+            if (phoneNumber.ToLower() == "q")
+                break;
+
+            trimNumber = Regex.Replace(phoneNumber, @"\s", "");
+
+            if (trimNumber.Length < 9)
+            {
+                Console.WriteLine("Invalid Phone Number");
+                continue;
+            }
+            else
+            {
+                // Standardize the phone number
+                standardPhoneNumber = Regex.Replace(trimNumber, pattern, "+94$2");
+                Console.WriteLine($"Is your phone number is: {standardPhoneNumber} Press 'y' for yes or 'n' for input again");
+                string responese = Console.ReadLine().Trim();
+
+                if (responese == "y")
+                    Console.WriteLine("Standardized phone number: " + standardPhoneNumber);
+            }
+        }
     }
 }
