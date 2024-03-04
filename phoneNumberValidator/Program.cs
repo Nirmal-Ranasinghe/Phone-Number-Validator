d41816098c4cb75de30efb0d69ef098824f95043
Nirmal Ranasinghe <NirmalRa@brownsgroup.com>
2024-03-04 12:30:30 +0530

    top level statment

--- phoneNumberValidator/Program.cs
+++ phoneNumberValidator/Program.cs
@@ -1,40 +1,34 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-class Program
+string pattern = @"^(0|\+?94)?([0-9]{9})$";
+string phoneNumber;
+string trimNumber;
+string standardPhoneNumber;
+string responese;
+
+while (true)
 {
-    static void Main(string[] args)
+    Console.Write("Enter the phone number (enter 'q' to quit): ");
+    phoneNumber = Console.ReadLine().Trim();
+
+    if (phoneNumber.ToLower() == "q")
+        break;
+
+    trimNumber = Regex.Replace(phoneNumber, @"\s", "");
+
+    if (trimNumber.Length < 9 || trimNumber.Length > 10)
     {
-        string pattern = @"^(0|\+?94)?([0-9]{9})$";
-        string phoneNumber;
-        string trimNumber;
-        string standardPhoneNumber;
-        string responese;
+        Console.WriteLine("Invalid Phone Number");
+        continue;
+    }
+    else
+    {
+        standardPhoneNumber = Regex.Replace(trimNumber, pattern, "+94$2");
+        Console.WriteLine($"Is the number is correct: {standardPhoneNumber} Press 'y' for yes or 'n' for input again");
+        responese = Console.ReadLine().Trim();
 
-        while (true)
-        {
-            Console.Write("Enter the phone number (enter 'q' to quit): ");
-            phoneNumber = Console.ReadLine().Trim();
-
-            if (phoneNumber.ToLower() == "q")
-                break;
-
-            trimNumber = Regex.Replace(phoneNumber, @"\s", "");
-
-            if (trimNumber.Length < 9 || trimNumber.Length > 10)
-            {
-                Console.WriteLine("Invalid Phone Number");
-                continue;
-            }
-            else
-            {
-                standardPhoneNumber = Regex.Replace(trimNumber, pattern, "+94$2");
-                Console.WriteLine($"Is the number is correct: {standardPhoneNumber} Press 'y' for yes or 'n' for input again");
-                responese = Console.ReadLine().Trim();
-
-                if (responese == "y")
-                    Console.WriteLine("Standardized phone number: " + standardPhoneNumber);
-            }
-        }
+        if (responese == "y")
+            Console.WriteLine("Standardized phone number: " + standardPhoneNumber);
     }
 }
