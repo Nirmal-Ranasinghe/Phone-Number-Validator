721582a611c4ff29b950a798cd4d9562e8eb4de6
Nirmal Ranasinghe <NirmalRa@brownsgroup.com>
2024-03-04 09:34:57 +0530

    Modify

--- phoneNumberValidator/Program.cs
+++ phoneNumberValidator/Program.cs
@@ -1,10 +1,20 @@
 ﻿using static System.Net.Mime.MediaTypeNames;
 using System.Text.RegularExpressions;
+using System.Reflection.Metadata;
+using System;
 
-Console.Write("Enter the phone number: ");
-String phoneNumber = Console.ReadLine();
 
-string trimNumber = Regex.Replace(phoneNumber, @"\s", "");
+class PhoneNumber {
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the phone number: ");
+        string phoneNumber = Console.ReadLine();
+        string trimNumber = Regex.Replace(phoneNumber, @"\s", "");
 
-string message = (trimNumber.Length < 0 || trimNumber == null) ? "Please enter your number" : $"You number is {trimNumber}";
-Console.Write(message);
+        string pattern = @"^(0|\+?94)?([0-9]{9})$";
+
+        string standardPhoneNumber = Regex.Replace(trimNumber, pattern, "94$2");
+
+        Console.WriteLine("Standardized phone number: " + standardPhoneNumber);
+    }
+}
